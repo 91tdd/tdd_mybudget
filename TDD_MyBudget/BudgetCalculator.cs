469d6be4cfcbeb1acf188de2266d7e3879e05b2c
hatelove <jay.hatelove@gmail.com>
2018-06-10 01:38:58 +0800

    use Sum() instead of foreach aggregate

--- TDD_MyBudget/BudgetCalculator.cs
+++ TDD_MyBudget/BudgetCalculator.cs
@@ -35,26 +35,7 @@ namespace TDD_MyBudget
 
         private decimal TotalBudgetOfMultiMonthsPeriod(Period period, List<Budget> budgets)
         {
-            decimal totalBudget = 0;
-            foreach (var budget in budgets)
-            {
-                totalBudget += budget.EffectiveAmount(period);
-            }
-
-            return totalBudget;
-            for (int year = period.Start.Year; year <= period.End.Year; year++)
-            {
-                for (int month = period.Start.Month; month <= period.End.Month; month++)
-                {
-                    var budget = budgets.FirstOrDefault(x => x.Month == $"{year:0000}{month:00}");
-                    if (budget != null)
-                    {
-                        totalBudget += budget.EffectiveAmount(period);
-                    }
-                }
-            }
-
-            return totalBudget;
+            return budgets.Sum(b => b.EffectiveAmount(period));
         }
     }
 }
