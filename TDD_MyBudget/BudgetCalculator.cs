0f1e5848080041f87216cf1e4612a8e0490cacae
hatelove <jay.hatelove@gmail.com>
2018-06-10 01:09:39 +0800

    extract method TotalBudgetOfMultiMonthsPeriod()

--- TDD_MyBudget/BudgetCalculator.cs
+++ TDD_MyBudget/BudgetCalculator.cs
@@ -19,9 +19,22 @@ namespace TDD_MyBudget
             var budgets = _repo.GetBudget();
             if (period.IsSameMonth())
             {
-                return EffectiveAmount(budgets, period);
+                var budget = budgets.FirstOrDefault(x => x.Month == $"{period.Start:yyyyMM}");
+                if (budget == null)
+                {
+                    return 0;
+                }
+
+                return period.Days() * budget.DailyAmount();
             }
 
+            var totalBudget = TotalBudgetOfMultiMonthsPeriod(period, budgets);
+
+            return totalBudget;
+        }
+
+        private decimal TotalBudgetOfMultiMonthsPeriod(Period period, List<Budget> budgets)
+        {
             decimal totalBudget = 0;
             for (int year = period.Start.Year; year <= period.End.Year; year++)
             {
